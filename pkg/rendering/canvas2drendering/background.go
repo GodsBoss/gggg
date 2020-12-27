3a6130b734e652c0396509f38c2ba80a1ad8cd87
Timo Reitz <godsboss@gmx.de>
2020-12-27 18:56:02 +0100

    Add renderer for monochrome backgrounds.

---
+++ pkg/rendering/canvas2drendering/background.go
@@ -0,0 +1,19 @@
+// +build js,wasm
+
+package canvas2drendering
+
+import (
+	"github.com/GodsBoss/gggg/pkg/dom"
+)
+
+func MonochromeBackground(color string) (RenderFunc, error) {
+	fillStyle, err := dom.NewColorCanvasFillStyle(color)
+	if err != nil {
+		return nil, err
+	}
+	return func(ctx *dom.Context2D) {
+		ctx.SetFillStyle(fillStyle)
+		w, h := ctx.Size()
+		ctx.FillRect(0, 0, w, h)
+	}, nil
+}
