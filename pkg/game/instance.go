f221f96dfe77502e7e9bf7032353e8d9763ff002
Timo Reitz <godsboss@gmx.de>
2023-01-07 13:06:56 +0100

    Actually lock and unlock instead of only lock (seems to be a NOP, else it wouldn't work).

--- pkg/game/instance.go
+++ pkg/game/instance.go
@@ -20,17 +20,17 @@ func (instance *Instance[Data]) Data() Data {
 }
 
 func (instance *Instance[Data]) ReceiveKeyEvent(event keyboard.Event) {
-	defer instance.unlockAfterLock()
+	defer instance.unlockAfterLock()()
 	instance.nextState(instance.currentState().ReceiveKeyEvent(instance.data, event))
 }
 
 func (instance *Instance[Data]) ReceiveMouseEvent(event mouse.Event) {
-	defer instance.unlockAfterLock()
+	defer instance.unlockAfterLock()()
 	instance.nextState(instance.currentState().ReceiveMouseEvent(instance.data, event))
 }
 
 func (instance *Instance[Data]) ReceiveTickEvent(event tick.Event) {
-	defer instance.unlockAfterLock()
+	defer instance.unlockAfterLock()()
 	instance.nextState(instance.currentState().ReceiveTickEvent(instance.data, event))
 }
 
