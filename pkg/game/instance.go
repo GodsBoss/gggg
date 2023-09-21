2effff4a280528697eebd258c6ee43e908c0d53b
Timo Reitz <godsboss@gmx.de>
2023-09-21 16:47:25 +0200

    Document instance.

--- pkg/game/instance.go
+++ pkg/game/instance.go
@@ -8,6 +8,8 @@ import (
 	"github.com/GodsBoss/gggg/v2/pkg/event/tick"
 )
 
+// Instance represents a game instance. It wraps the data together with a bunch of states and
+// takes and passes events.
 type Instance[Data any] struct {
 	states         map[StateID]State[Data]
 	data           Data
@@ -15,25 +17,31 @@ type Instance[Data any] struct {
 	mutex          sync.Mutex
 }
 
+// Data exposes the instance's data.
 func (instance *Instance[Data]) Data() Data {
 	return instance.data
 }
 
+// ReceiveKeyEvent passes a key event to the current state.
 func (instance *Instance[Data]) ReceiveKeyEvent(event keyboard.Event) {
 	defer instance.unlockAfterLock()()
 	instance.nextState(instance.currentState().ReceiveKeyEvent(instance.data, event))
 }
 
+// ReceiveMouseEvent passes a mouse event to the current state.
 func (instance *Instance[Data]) ReceiveMouseEvent(event mouse.Event) {
 	defer instance.unlockAfterLock()()
 	instance.nextState(instance.currentState().ReceiveMouseEvent(instance.data, event))
 }
 
+// ReceiveTickEvent passes a tick event to the current state.
 func (instance *Instance[Data]) ReceiveTickEvent(event tick.Event) {
 	defer instance.unlockAfterLock()()
 	instance.nextState(instance.currentState().ReceiveTickEvent(instance.data, event))
 }
 
+// nextState sets the instance to the next state. If the next state differs from the current state,
+// the next state's Init() method is called.
 func (instance *Instance[Data]) nextState(f func() (StateID, bool)) {
 	next, ok := f()
 	if !ok {
@@ -48,11 +56,13 @@ func (instance *Instance[Data]) nextState(f func() (StateID, bool)) {
 	instance.currentState().Init(instance.data)
 }
 
+// unlockAfterLock locks the instance and returns a function that unlocks it. Use with defer for safe unlocking.
 func (instance *Instance[Data]) unlockAfterLock() func() {
 	instance.mutex.Lock()
 	return instance.mutex.Unlock
 }
 
+// currentState returns the current state.
 func (instance *Instance[Data]) currentState() State[Data] {
 	return instance.states[instance.currentStateID]
 }
