65c4d2b8427f676193c693dcd84b7f6412193455
Timo Reitz <godsboss@gmx.de>
2023-09-21 21:26:25 +0200

    Add unit vector constructors.

--- pkg/vector/vector2d/numeric.go
+++ pkg/vector/vector2d/numeric.go
@@ -22,3 +22,23 @@ func Invert[T constraints.Float | constraints.Integer | constraints.Complex](v V
 func Scaled[T constraints.Float | constraints.Integer | constraints.Complex](v Vector[T], f T) Vector[T] {
 	return Cartesian(f*v.x, f*v.y)
 }
+
+// Left returns a unit vector that points to the left.
+func Left[T constraints.Float | constraints.Signed | constraints.Complex]() Vector[T] {
+	return Cartesian[T](-1, 0)
+}
+
+// Right returns a unit vector that points to the right.
+func Right[T constraints.Float | constraints.Integer | constraints.Complex]() Vector[T] {
+	return Cartesian[T](1, 0)
+}
+
+// Up returns a unit vector that points up.
+func Up[T constraints.Float | constraints.Signed | constraints.Complex]() Vector[T] {
+	return Cartesian[T](0, -1)
+}
+
+// Down returns a unit vector that points down.
+func Down[T constraints.Float | constraints.Integer | constraints.Complex]() Vector[T] {
+	return Cartesian[T](0, 1)
+}
